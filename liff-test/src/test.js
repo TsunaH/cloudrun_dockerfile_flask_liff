8fedcf9829062ec78e34fef3920e5e49b26742f4
TsunenoriHenna <tsunenori.henna@dragon.jp>
2025-01-31 15:02:46 +0900

    本番用LIFF IDに変更

--- liff-test/src/test.js
+++ liff-test/src/test.js
@@ -4,9 +4,9 @@ import App from './App.vue';
 import liff from "@line/liff";
 
 // ローカルテスト用
-let liffid = "2006831755-QxVm4KvW";
+//let liffid = "2006831755-QxVm4KvW";
 // LINE動作確認要
-// let liffid = "2006831755-QEdgjOA8";
+let liffid = "2006831755-QEdgjOA8";
 
 liff.init({
   liffId: liffid, // Use own liffId
