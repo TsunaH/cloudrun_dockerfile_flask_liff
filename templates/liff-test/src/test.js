0c4effc0f5b4713889560a6307435689d8b5f24f
TsunenoriHenna <tsunenori.henna@dragon.jp>
2025-02-03 18:56:56 +0900

    修正

--- templates/liff-test/src/test.js
+++ templates/liff-test/src/test.js
@@ -8,20 +8,17 @@ import liff from "@line/liff";
 // LINE動作確認要
 let liffid = "2006831755-QEdgjOA8";
 
-liff.init({
-  liffId: liffid, // Use own liffId
-});
-
-liff.ready.then(() => {
-  if (liffid == "2006831755-QEdgjOA8") {
+liff
+  .init({
+    liffId: liffid, // Use own liffId
+  })
+  .then(() => {
     liff.sendMessages([
-      {
-        type: "text",
-        text: "Hello World"
-      }
+    {
+      type: "text",
+      text: "Hello World"
+    }
     ]);
-  }
-  console.log('henna')
-});
+  });
 
 createApp(App).mount('#app');
