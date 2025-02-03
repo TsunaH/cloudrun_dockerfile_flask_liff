81cfa5f8a970e3e5bd8eb8c8ee1aaebb51a1bd9e
TsunenoriHenna <tsunenori.henna@dragon.jp>
2025-02-03 19:22:42 +0900

    修正

--- templates/liff-test/src/test.js
+++ templates/liff-test/src/test.js
@@ -3,6 +3,9 @@ import App from './App.vue';
 
 import liff from "@line/liff";
 
+createApp(App).mount('#app');
+
+
 // ローカルテスト用
 //let liffid = "2006831755-QxVm4KvW";
 // LINE動作確認要
@@ -25,6 +28,5 @@ liff
 liff
   .init({
     //liffId: liffid, // Use own liffId
-    liffId: "2006831755-QEdgjOA8"
+    liffId: "2006831755-QEdgjOA8",
   });
-createApp(App).mount('#app');
