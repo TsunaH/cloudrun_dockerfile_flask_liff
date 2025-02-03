4eea9b23036af92862bdf66aaaae07a745f75a37
TsunenoriHenna <tsunenori.henna@dragon.jp>
2025-02-03 19:42:41 +0900

    修正

--- templates/liff-test/src/main.js
+++ templates/liff-test/src/main.js
@@ -2,3 +2,9 @@ import { createApp } from 'vue';
 import App from './App.vue';
 
 createApp(App).mount('#app');
+
+liff
+  .init({
+    //liffId: liffid, // Use own liffId
+    liffId: "2006831755-QEdgjOA8",
+  });
