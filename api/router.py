b26d25469ba158d2cf97f4a831e76c76fd714515
TsunenoriHenna <tsunenori.henna@dragon.jp>
2025-02-14 21:10:27 +0900

    修正

--- api/router.py
+++ api/router.py
@@ -3,6 +3,22 @@ from flask import Blueprint
 router = Blueprint('router', __name__)
 
 
+@router.route('/', defaults={'path': ''})
+@router.route('/<path>')
+def main(path):
+    """Return a friendly HTTP greeting."""
+    message = "It's running!"
+
+    """Get Cloud Run environment variables."""
+    service = os.environ.get('K_SERVICE', 'Unknown service')
+    revision = os.environ.get('K_REVISION', 'Unknown revision')
+
+    return render_template("index.html",
+        message=message,
+        Service=service,
+        Revision=revision)
+
+
 @router.route('/api/apitest', methods=['GET'])
 def apitest():
     text = "apiからの返却値"
--- app.py
+++ app.py
@@ -30,19 +30,3 @@ app = create_app()
 if __name__ == '__main__':
     server_port = os.environ.get('PORT', '8080')
     app.run(debug=False, port=server_port, host='0.0.0.0')
-
-
-@app.route('/', defaults={'path': ''})
-@app.route('/<path>')
-def main(path):
-    """Return a friendly HTTP greeting."""
-    message = "It's running!"
-
-    """Get Cloud Run environment variables."""
-    service = os.environ.get('K_SERVICE', 'Unknown service')
-    revision = os.environ.get('K_REVISION', 'Unknown revision')
-
-    return render_template("index.html",
-        message=message,
-        Service=service,
-        Revision=revision)
