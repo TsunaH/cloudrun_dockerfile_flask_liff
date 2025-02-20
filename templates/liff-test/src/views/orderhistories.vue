fa29bceabdcb3ebf6e96054666b3849b0b12a860
TsunenoriHenna <tsunenori.henna@dragon.jp>
2025-02-20 20:47:52 +0900

    修正

--- templates/liff-test/src/views/orderhistories.vue
+++ templates/liff-test/src/views/orderhistories.vue
@@ -38,21 +38,22 @@
   
 <template>
   <form v-on:submit.prevent="onUpdate">
-
     <dl>
       <template
         v-for="orderhistory in orderhistories"
-        v-bind:key="KEY"> 
+        v-bind:key="KEY"
+        style="border: 1px;"
+        > 
         <dt>■注文番号&nbsp;</dt>
         <dd>&nbsp;&nbsp;{{orderhistory.order_id}}</dd>
         <dt>注文日&nbsp;</dt>
-        <dd>{{orderhistory.email}}</dd>
+        <dd>{{orderhistory.order_regist_day}}</dd>
         <dt>■ご注文商品（数量）&nbsp;</dt>
-          <template
+          <div
             v-for="product_info in orderhistory.product_infos"
             v-bind:key="KEY">
             {{ product_info.product_name }}({{ product_info.order_qty }})
-          </template>
+          </div>
         <dt>■ご注文金額&nbsp;</dt>
         <dd>&nbsp;&nbsp;{{orderhistory.order_amount}}</dd>
         <dt>■ご注文の状態&nbsp;</dt>
