4abb697011e72977ea68e2e6b6fb6ef3b991dd3e
TsunenoriHenna <tsunenori.henna@dragon.jp>
2025-02-25 13:57:40 +0900

    修正

--- templates/liff-test/src/views/orderhistories.vue
+++ templates/liff-test/src/views/orderhistories.vue
@@ -28,8 +28,8 @@
       .then(function(result) {
         // データ処理が正常の場合
         orderhistories.value = result.data;
-        alert("in then");
-      alert(JSON.stringify(result));
+        //alert("in then");
+        //alert(JSON.stringify(result));
         // エラーの場合
         alert(JSON.stringify(orderhistories.value));
       })
@@ -43,21 +43,21 @@
         v-for="orderhistory in orderhistories"
         v-bind:key="KEY"
         style="border: 1px solid #333333;"> 
-        <dt v-if="orderhistory.control_items.order_id==constvalue['DISP_ONLY']">■注文番号&nbsp;</dt>
-        <dd v-if="orderhistory.control_items.order_id==constvalue['DISP_ONLY']">&nbsp;&nbsp;{{orderhistory.order_id}}</dd>
-        <dt v-if="orderhistory.control_items.order_regist_day==constvalue['DISP_ONLY']">■注文日&nbsp;</dt>
-        <dd v-if="orderhistory.control_items.order_regist_day==constvalue['DISP_ONLY']">&nbsp;&nbsp;{{orderhistory.order_regist_day}}</dd>
-        <dt v-if="orderhistory.control_items.product_info==constvalue['DISP_ONLY']">■ご注文商品（数量）&nbsp;</dt>
+        <dt v-if="orderhistories.control_items.order_id==constvalue['DISP_ONLY']">■注文番号&nbsp;</dt>
+        <dd v-if="orderhistories.control_items.order_id==constvalue['DISP_ONLY']">&nbsp;&nbsp;{{orderhistory.order_id}}</dd>
+        <dt v-if="orderhistories.control_items.order_regist_day==constvalue['DISP_ONLY']">■注文日&nbsp;</dt>
+        <dd v-if="orderhistories.control_items.order_regist_day==constvalue['DISP_ONLY']">&nbsp;&nbsp;{{orderhistory.order_regist_day}}</dd>
+        <dt v-if="orderhistories.control_items.product_info==constvalue['DISP_ONLY']">■ご注文商品（数量）&nbsp;</dt>
           <div
             v-for="product_info in orderhistory.product_infos"
             v-bind:key="KEY"
-            v-if="orderhistory.control_items.product_info==constvalue['DISP_ONLY']">
+            v-if="orderhistories.control_items.product_info==constvalue['DISP_ONLY']">
             &nbsp;&nbsp;{{ product_info.product_name }}({{ product_info.order_qty }})
           </div>
-        <dt v-if="orderhistory.control_items.order_amount==constvalue['DISP_ONLY']">■ご注文金額&nbsp;</dt>
-        <dd v-if="orderhistory.control_items.order_amount==constvalue['DISP_ONLY']">&nbsp;&nbsp;{{orderhistory.order_amount}}</dd>
-        <dt v-if="orderhistory.control_items.shipment_detail_status==constvalue['DISP_ONLY']">■ご注文の状態&nbsp;</dt>
-        <dd v-if="orderhistory.control_items.shipment_detail_status==constvalue['DISP_ONLY']">&nbsp;&nbsp;{{orderhistory.shipment_detail_status}}</dd>
+        <dt v-if="orderhistories.control_items.order_amount==constvalue['DISP_ONLY']">■ご注文金額&nbsp;</dt>
+        <dd v-if="orderhistories.control_items.order_amount==constvalue['DISP_ONLY']">&nbsp;&nbsp;{{orderhistory.order_amount}}</dd>
+        <dt v-if="orderhistories.control_items.shipment_detail_status==constvalue['DISP_ONLY']">■ご注文の状態&nbsp;</dt>
+        <dd v-if="orderhistories.control_items.shipment_detail_status==constvalue['DISP_ONLY']">&nbsp;&nbsp;{{orderhistory.shipment_detail_status}}</dd>
       </div>
     </dl>
   </form>
